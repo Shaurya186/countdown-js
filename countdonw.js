19323c748c29c202190af8c8c9ce6e38cbb56ca2
SH4URYA.Singh838 <69413964+Shaurya186@users.noreply.github.com>
2020-08-26 22:50:56 +0530

    Add files via upload

---
+++ countdonw.js
@@ -0,0 +1,17 @@
+let countDownFromDate = new Date('Feb 1, 2020 12:00:00').toDateString();
+let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
+let x = setInterval(function(){
+let now = new Date().getTime();
+let distance = countDownDate - now;
+let days = Math.floor(distance / (1000 * 60 *60 * 24));
+let hours = Math.floor((distance % (1000 * 60 *60*24)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById('countdown-from').innerHTML = `${countDownFromDate}`
+document.getElementById('root').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+if (distance < 0){
+    clearInterval(x);
+    document.getElementById('root').innerHTML = 'Timer Expired';
+}
+
+}, 1000);
